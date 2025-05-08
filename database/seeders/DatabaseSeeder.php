b891982cd3e0c908ba945d06774092eae6705630
root <root@vig-srv-botmap.cercatarga.local>
2025-05-08 11:57:57 +0200

    Versione 1.0.0 - Modifica utente iniziale

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'user',
             'email' => 'test@example.com',
             'password' => Hash::make("user123")
         ]);
